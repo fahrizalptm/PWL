b14e402cd73d3f4ce2e6c4011ff01d53bb33311b
fahrizalptm <113316582+fahrizalptm@users.noreply.github.com>
2023-04-30 17:47:40 +0700

    Add files via upload

---
+++ pwl/pricelist.php
@@ -0,0 +1,15 @@
+<?php 
+  include "./components/header.php";
+?>
+<main>
+  <div class="pricelist">
+    <h2>Pricelist Joki</h2>
+    <div class="prices">
+        <img src="./img/kirii.png" alt="kirii">
+        <img src="./img/kanann.png" alt="kanann">
+    </div>
+  </div>
+</main>
+<?php 
+  include "./components/footer.php";
+?>
